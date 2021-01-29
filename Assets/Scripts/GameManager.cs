eb32b7acaa46809469e8e7c8bb9300e5a73c68ad
N-bred <thehardyboiz1@gmail.com>
2021-01-29 04:24:59 -0500

    First commit, little bit late but i've advanced a lot by now, basic player movement, scenarios with images, game manager script initialized, door interaction setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Dictionary<Collider2D, GameObject> doors;
+    public GameObject playPanel;
+    public Text interactions;
+    public GameObject blueDoor;
+    private bool isMainScene = true;
+    public static GameManager instance { get; set; }
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void InteractWithDoor(Collider2D door)
+    {
+
+        Debug.Log("Interacting...");
+    }
+
+    public void ShowDoorMessage(Collider2D door, bool isStaying)
+    {
+        if (isStaying)
+        {
+            interactions.text = $"Press E to interact with: {door.name}";
+
+        }
+        else
+        {
+            interactions.text = "";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float horizontalAxis;
+    private float verticalAxis;
+    public float movSpeed;
+    private Rigidbody2D rigidBody;
+    public int lifes;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Physics2D.gravity = Vector2.zero;
+        rigidBody.gravityScale = 0.0f;
+        rigidBody.velocity = new Vector2(horizontalAxis * movSpeed, verticalAxis * movSpeed);
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag == "Door")
+        {
+            GameManager.instance.ShowDoorMessage(collision.collider, true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.instance.InteractWithDoor(collision.collider);
+            }
+        }
+      
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Door")
+        {
+            GameManager.instance.ShowDoorMessage(collision.collider, false);
+        }
+    }
+}
